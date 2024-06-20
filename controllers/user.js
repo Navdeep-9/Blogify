8ecb6159ac8dae476798033be9dd596eab006be0
navdeep@9 <navdeepsingh.stealth@gmail.com>
2024-06-20 18:17:42 +0530

    V9

--- controllers/user.js
+++ controllers/user.js
@@ -231,7 +231,43 @@ async function getuserforforgotpassword(req, res) {
             to: [`${email}`],
             subject: 'OTP',
             text: `Your OTP is`,
-            html: `<h1> you otp is ${randomNumber}</h1>`
+            html: `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <style>
+        body {
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            background-color: orange;
+            padding: 50px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            text-align: center;
+        }
+        .container h1 {
+            margin: 0;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome Here is the OTP </h1>
+        <h2>${randomNumber}</h2>
+    </div>
+</body>
+</html>
+`
 
         })
         // console.log('email sent');
